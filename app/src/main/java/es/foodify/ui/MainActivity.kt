25548958f0cbf046abd05d36989c6ecf8c83384b
Victor de Francisco <vfrancisco@idealista.com>
2018-08-01 22:03:58 +0200

    Play with bottom navigation bar icons

--- app/src/main/java/es/foodify/ui/MainActivity.kt
+++ app/src/main/java/es/foodify/ui/MainActivity.kt
@@ -1,24 +1,23 @@
-package es.foodify
+package es.foodify.ui
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import es.foodify.R.id
+import es.foodify.R.layout
+import kotlinx.android.synthetic.main.activity_main.navigation
 
 class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            id.navigation_home -> {
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+            id.navigation_dashboard -> {
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            id.navigation_notifications -> {
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -27,7 +26,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
